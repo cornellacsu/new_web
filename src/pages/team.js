1fd0914219d63ed4b4daa5a9a3812091821a33cc
sandy <sf373@cornell.edu>
2019-06-03 02:52:45 -0400

    officer stuff

--- src/pages/team.js
+++ src/pages/team.js
@@ -1,24 +1,39 @@
 import React from "react"
-import { Card, Image } from 'semantic-ui-react'
+import { Grid, Card, Image } from 'semantic-ui-react'
 import { StaticQuery, graphql } from "gatsby"
+import Navbar from "../components/navbar"
 import data from '../data/team.json'
 
 const officers = data.officers;
-officers.map(officer => console.log(officer))
 
+const cols = 4;
+
+// const createOfficerGrid = (cols) => {
+//    var i,j;
+//    for( i = 0; i < officers.len(); i+= cols){
+//       console.log(cols,"cols")
+//    }
+// }
 
 const Team = () => (
    <div>
+   <Navbar />
    <p>team page</p>
-   {officers.map(officer => 
-      <Card textAlign="center" key={officer.email}>
+   <Grid columns={cols}> 
+   {
+      officers.map(officer => {
+      var grid = [];
+
+      grid.push((<Card textAlign="center" key={officer.email}>
          <Image src={officer.img} circular />
          <Card.Content>
          <Card.Header>{`${officer.name} '${officer.year}`}</Card.Header> 
          <Card.Meta>{officer.team}</Card.Meta>
          </Card.Content>
-      </Card>
-      )}
+      </Card>))
+      return grid
+      })}
+   </Grid>
    </div>
 )
 
